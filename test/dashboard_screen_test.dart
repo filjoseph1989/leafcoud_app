7dc3b35b1cff721e098705a906eb72f70a26006f
fil <fil@creativextechlabsinc.com>
2026-03-03 00:27:24 +0800

    test: Add BucketControlNotifier to DashboardScreen tests

--- test/dashboard_screen_test.dart
+++ test/dashboard_screen_test.dart
@@ -6,6 +6,7 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_leafcloud_app/dashboard_screen.dart';
 import 'package:flutter_leafcloud_app/notifiers/sensor_data_notifier.dart';
+import 'package:flutter_leafcloud_app/notifiers/bucket_control_notifier.dart';
 import 'package:flutter_leafcloud_app/services/api_service.dart';
 import 'package:flutter_leafcloud_app/models/sensor_data.dart';
 
@@ -22,12 +23,16 @@ void main() {
   testWidgets('DashboardScreen shows loading indicator initially', (WidgetTester tester) async {
     // Provide a completion for the initial fetch that startPolling will trigger
     when(mockApiService.fetchSensorData()).thenAnswer((_) async => Completer<SensorData>().future);
+    when(mockApiService.fetchActiveBucketStatus()).thenAnswer((_) async => 'None');
 
     await tester.pumpWidget(
       MultiProvider(
         providers: [
           ChangeNotifierProvider(
             create: (_) => SensorDataNotifier(apiService: mockApiService),
+          ),
+          ChangeNotifierProvider(
+            create: (_) => BucketControlNotifier(apiService: mockApiService),
           ),
         ],
         child: const MaterialApp(home: DashboardScreen()),
