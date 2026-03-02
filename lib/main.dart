1076f2d0c66df0e2dbad26b6ca6ef3101ddd9b71
fil <fil@creativextechlabsinc.com>
2026-03-02 23:46:05 +0800

    feat(provider): Register BucketControlNotifier in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,27 @@ import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_leafcloud_app/login_screen.dart';
 import 'package:flutter_leafcloud_app/notifiers/sensor_data_notifier.dart';
+import 'package:flutter_leafcloud_app/notifiers/bucket_control_notifier.dart';
 import 'package:flutter_leafcloud_app/services/api_service.dart';
 
 void main() {
+  final httpClient = http.Client();
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(
           create: (_) => SensorDataNotifier(
             apiService: ApiService(
-              client: http.Client(),
+              client: httpClient,
               baseUrl: 'http://192.168.1.7:8000',
+            ),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => BucketControlNotifier(
+            apiService: ApiService(
+              client: httpClient,
+              baseUrl: 'http://192.168.1.2:8000',
             ),
           ),
         ),
