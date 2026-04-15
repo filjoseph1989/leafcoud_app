81eb194516af905da2ce45600763c5356e1a0312
fil <fil@creativextechlabsinc.com>
2026-04-15 22:58:24 +0800

    feat(macos): set mobile-like window size and position

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,7 +4,17 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
+    var windowFrame = self.frame
+
+    if let screen = NSScreen.main {
+      let screenFrame = screen.visibleFrame
+      let height = screenFrame.height
+      let width: CGFloat = 320 // Mobile-like width
+      let x = screenFrame.origin.x + (screenFrame.width - width) / 2
+      let y = screenFrame.origin.y
+      windowFrame = NSRect(x: x, y: y, width: width, height: height)
+    }
+
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
