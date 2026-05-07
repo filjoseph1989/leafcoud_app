f5a2513ca10af48381acc19e7698c3e3d19fcf18
fil <fil@creativextechlabsinc.com>
2026-05-08 03:30:18 +0800

    chore: remove Image Cropper from drawer menu

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -6,7 +6,6 @@ import 'package:flutter_leafcloud_app/alerts_screen.dart';
 import 'package:flutter_leafcloud_app/image_gallery_screen.dart';
 import 'package:flutter_leafcloud_app/data_gathering_screen.dart';
 import 'package:flutter_leafcloud_app/trash_screen.dart';
-import 'package:flutter_leafcloud_app/image_cropper_screen.dart';
 import 'package:flutter_leafcloud_app/notifiers/sensor_data_notifier.dart';
 import 'package:flutter_leafcloud_app/models/sensor_data.dart';
 
@@ -98,7 +97,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
               ),
             ),
             _buildDrawerItem(context, Icons.analytics_outlined, 'Data Gathering', const DataGatheringScreen()),
-            _buildDrawerItem(context, Icons.crop_rounded, 'Image Cropper', const ImageCropperScreen()),
             _buildDrawerItem(context, Icons.photo_library_outlined, 'Image Gallery', const ImageGalleryScreen()),
             _buildDrawerItem(context, Icons.delete_outline_rounded, 'Trash', const TrashScreen()),
             const Divider(indent: 20, endIndent: 20),
